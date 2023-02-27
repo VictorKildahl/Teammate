d8fc9f54e62dff7446ec64d43954f9a7e4cab0a2
Lasse Mosel <lmos@JYSK.com>
2023-02-27 13:44:22 +0100

    navigation

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,15 +1,12 @@
 import { MemoryRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from 'screens/Home';
 import './App.css';
-
-function Hello() {
-  return <h1 className="text-5xl">Hello world!</h1>;
-}
 
 export default function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Hello />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </Router>
   );
---
+++ src/screens/Home.tsx
@@ -0,0 +1,10 @@
+import BottomNavigation from 'components/BottomNavigation';
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <BottomNavigation />
+    </div>
+  );
+}
