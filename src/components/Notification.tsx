c27f055b5ff5ad12a4de90bb512d6a325d06c76c
Victor Kildahl Petersen <vkp@jysk.com>
2023-02-28 10:49:11 +0100

    home screen

--- src/components/Notification.tsx
+++ src/components/Notification.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import donut from '../../assets/icons/donut.svg';
 import cutlery from '../../assets/icons/cutlery.svg';
+import donut from '../../assets/icons/donut.svg';
 
 type NotificationProps = {
   text: string;
   type: 'cake' | 'lunch';
+  time?: string;
 };
 
-export default function Notification({ text, type }: NotificationProps) {
+export default function Notification({ text, type, time }: NotificationProps) {
   return (
-    <div className="flex justify-center items-center w-1/2 h-20 bg-blue-100 mt-4 rounded-lg">
+    <div className="flex items-center justify-center w-1/2 h-20 mt-4 bg-blue-100 rounded-lg">
       <div className="px-4">
         <img
           alt="icon"
           src={type === 'cake' ? donut : cutlery}
-          className="w-12 group-hover:scale-110 ease-in duration-100"
+          className="w-12 duration-100 ease-in group-hover:scale-110"
         />
       </div>
       <div className="">{text}</div>
+      {time && <div className="pl-4">{time}</div>}
     </div>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,15 +1,34 @@
 import Notification from 'components/Notification';
+import { firestore } from 'constants/Firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
-  return (
-    <div className="flex justify-center pt-8 flex-col items-center">
-      <div className="font-bold text-4xl">Home</div>
+  const [data, setData] = useState([]);
 
-      <Notification text="Cake in kitchen 5.1.13" type="cake" />
-      <Notification text="Victor is going to lunch now" type="lunch" />
-      <Notification text="Cake in kitchen 5.1.13" type="cake" />
-      <Notification text="Victor is going to lunch now" type="lunch" />
-      <Notification text="Cake in kitchen 5.1.13" type="cake" />
+  async function getData() {
+    const array = [];
+    const colRef = collection(firestore, 'lunch');
+    const docsSnap = await getDocs(colRef);
+
+    docsSnap.forEach((doc) => {
+      array.push([doc.data().text, doc.data().time]);
+    });
+
+    setData(array);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div className="flex flex-col items-center justify-center pt-8">
+      <div className="text-4xl font-bold">Home</div>
+
+      {data.map((item) => (
+        <Notification type="lunch" text={item[0]} time={item[1]} />
+      ))}
     </div>
   );
 }
