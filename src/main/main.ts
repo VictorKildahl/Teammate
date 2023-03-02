169dd5e33cc84ef0feac83754091ba5a6a5603df
Victor Kildahl Petersen <vkp@jysk.com>
2023-03-02 10:47:24 +0100

    can now build

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,9 +8,22 @@ if (process.env.NODE_ENV === 'production') {
   sourceMapSupport.install();
 }
 
-const mb = menubar({
-  index: `http://localhost:1212/`,
-  icon: path.join(process.cwd(), './assets/icons/t.png'),
+function resolveHtmlPath(htmlFileName: string) {
+  if (process.env.NODE_ENV === 'development') {
+    const port = process.env.PORT || 1212;
+    const url = new URL(`http://localhost:${port}`);
+    url.pathname = htmlFileName;
+    return url.href;
+  }
+  return `file://${path.resolve(__dirname, '../renderer/', htmlFileName)}`;
+}
+
+menubar({
+  index: resolveHtmlPath('index.html'),
+  // index: `http://localhost:1212/`,
+  icon: app.isPackaged
+    ? path.join(process.resourcesPath, './assets/icons/t.png')
+    : path.join(process.cwd(), './assets/icons/t.png'),
   browserWindow: {
     width: 600,
     height: 700,
@@ -19,12 +32,6 @@ const mb = menubar({
     maxwidth: 600,
     maxheight: 700,
     useContentSize: true,
-    webPreferences: {
-      contextIsolation: false,
-      preload: app.isPackaged
-        ? path.join(__dirname, 'preload.js')
-        : path.join(__dirname, '../../.erb/dll/preload.js'),
-    },
   },
 });
 
